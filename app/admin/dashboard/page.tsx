58effc4000a7b731e85b54eedbbdefcebba8b967
Sangy <apple2951@gmail.com>
2025-05-30 20:18:30 +0800

    feat:finish admin logout function

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,11 +1,30 @@
+"use client";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
+import { signOut } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        router.push("/admin/login");
+      })
+      .catch((error) => {
+        console.error("Logout failed:", error);
+      });
+  };
+
   return (
     <>
       <nav className="flex justify-end p-10">
-        <Button>Logout</Button>
+        <Button className="cursor-pointer" onClick={handleLogout}>
+          Logout
+        </Button>
       </nav>
 
       <main className="h-screen px-48 py-5">
