2023892100106058d4e80e51e3cd3de202819c74
Sangy <apple2951@gmail.com>
2025-06-12 14:00:07 +0800

    chore: Clean up api folder

--- app/api/course.ts
+++ app/api/course.ts
@@ -1,6 +1,15 @@
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  setDoc,
+  doc,
+} from "firebase/firestore";
 import { db } from "@/lib/firebase";
-import { Course } from "../../types";
+import { addDays, format } from "date-fns";
+import { getCoaches } from "./coach";
+import { Course, Coach } from "@/types";
 
 export const getCourses = async () => {
   try {
@@ -13,5 +22,64 @@ export const getCourses = async () => {
   } catch (error) {
     console.error("Error fetching coaches:", error);
     throw new Error("Failed to fetch coaches.");
+  }
+};
+
+export const addCourseToFirebase = async (course: Course) => {
+  await setDoc(doc(db, "course", course.id), course);
+  console.log(
+    `[CREATE] 新課程：${course.coachName} - ${course.courseType} @ ${course.datetime}`
+  );
+};
+
+export const generateCourseFromCoach = async (days = 14) => {
+  const coaches: Coach[] = await getCoaches();
+  const newCourses: Course[] = [];
+  const today = new Date();
+
+  for (let i = 0; i < days; i++) {
+    const date = addDays(today, i);
+    const dayIndex = date.getDay();
+
+    for (const coach of coaches) {
+      for (const { courseType, time, weekday } of coach.availableTimes) {
+        if (Number(weekday) == dayIndex) {
+          const hour = Number(time.slice(0, 2));
+          const datetime = new Date(date);
+          datetime.setHours(hour, 0, 0, 0);
+          const iso = datetime.toISOString();
+
+          const courseQuery = query(
+            collection(db, "course"),
+            where("coachId", "==", coach.id),
+            where("datetime", "==", datetime)
+          );
+
+          const snapshot = await getDocs(courseQuery);
+          if (!snapshot.empty) {
+            console.log(
+              `[SKIP] 已存在課程：${coach.name} - ${courseType} @ ${datetime}`
+            );
+            continue;
+          }
+
+          const course = {
+            id: coach.id + iso,
+            coachId: coach.id,
+            coachName: coach.name,
+            coachDescription: coach.description,
+            courseType,
+            date: format(datetime, "yyyy-MM-dd"),
+            datetime: iso,
+            reservations: [],
+          };
+          newCourses.push(course as Course);
+        }
+      }
+    }
+  }
+
+  for (const course of newCourses) {
+    addCourseToFirebase(course);
   }
 };
