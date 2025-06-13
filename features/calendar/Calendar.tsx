3fa0aea8a9d761cbd357ffc7cabacac1daaee114
Sangy <apple2951@gmail.com>
2025-06-13 13:35:31 +0800

    feat: Change event color & change time zone to UTC

--- features/calendar/Calendar.tsx
+++ features/calendar/Calendar.tsx
@@ -1,32 +1,34 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import momentTimezonePlugin from "@fullcalendar/moment-timezone";
 
 const CalendarComponent = () => {
   return (
     <FullCalendar
-      plugins={[timeGridPlugin]}
+      plugins={[timeGridPlugin, momentTimezonePlugin]}
       initialView="timeGridAWeek"
       timeZone="Asia/Taipei"
-      height={500}
+      height={"auto"}
       views={{
         timeGridAWeek: { type: "timeGrid", duration: { days: 7 } },
       }}
       allDaySlot={false}
       slotMinTime={"09:00:00"}
       slotMaxTime={"22:00:00"}
-      slotDuration={{ hours: 1 }}
+      slotDuration={"00:30:00"}
+      eventColor="black"
       events={[
         {
           title: "Sample Event",
-          start: "2025-06-13T10:00:00",
-          end: "2025-06-13T12:00:00",
+          start: "2025-06-13T10:00:00Z",
+          end: "2025-06-13T12:00:00Z",
         },
-        {
-          title: "Other Event",
-          start: "2025-06-13T10:00:00",
-          end: "2025-06-13T12:00:00",
-        },
+        // {
+        //   title: "Other Event",
+        //   start: "2025-06-13T10:00:00",
+        //   end: "2025-06-13T12:00:00",
+        // },
       ]}
     />
   );
