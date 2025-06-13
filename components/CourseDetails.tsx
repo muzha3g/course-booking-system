d0bfa323f5100657e4b02df557d4df99f6f32181
Sangy <apple2951@gmail.com>
2025-06-13 15:27:53 +0800

    feat: Build CourseDetails component

---
+++ components/CourseDetails.tsx
@@ -0,0 +1,32 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function CourseDetails() {
+  return (
+    <div className="flex justify-center items-center w-full">
+      <Card className="w-1/2">
+        <CardHeader>
+          <CardTitle>Course Title</CardTitle>
+          <CardDescription>Coach name</CardDescription>
+          <CardAction>
+            2025/06/19 <br />
+            15:00 - 16:00
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>Students</p>
+          <Card>
+            <CardContent>Students Name</CardContent>
+          </Card>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
