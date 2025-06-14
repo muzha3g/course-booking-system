6c0acb04424ae1b87780230273e61b8032ce4676
Sangy <apple2951@gmail.com>
2025-06-15 01:00:14 +0800

    refactor: Move handleUserLogin to api folder

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -1,4 +1,4 @@
-import { auth } from "./firebase";
+import { auth } from "@/lib/firebase";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
--- components/CourseBookingButton.tsx
+++ components/CourseBookingButton.tsx
@@ -14,7 +14,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
-import { handleUserLogin } from "@/lib/auth";
+import { handleUserLogin } from "@/app/api/auth";
 import { Toaster } from "@/components/ui/sonner";
 import { toast } from "sonner";
 
