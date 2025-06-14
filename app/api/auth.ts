01ff0d554dbe9f51e7e855fa5647df9bcb105f3c
Sangy <apple2951@gmail.com>
2025-06-15 02:57:44 +0800

    feat: Handle case of same phone's number but different user name

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -17,14 +17,15 @@ export async function handleUserLogin(
   phone: string
 ) {
   const email = phoneToEmail(phone);
-  const password = name + phone;
+  // const password = name + phone;
+  const password = "default";
 
   try {
     // new user
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
-        updateProfile(auth.currentUser, { displayName: name });
+        updateProfile(auth.currentUser!, { displayName: name });
         console.log("User created successfully:", user);
         return addUserToCourseReservation(courseId, user.uid, name);
       })
@@ -38,29 +39,39 @@ export async function handleUserLogin(
           signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
               const user = userCredential.user;
-              console.log("signInWithEmailAndPassword: " + user.displayName);
+              updateProfile(auth.currentUser!, { displayName: name });
+              console.log("User changed display name: ", user.displayName);
+              console.log(
+                "signInWithEmailAndPassword Name: " + user.displayName
+              );
               return addUserToCourseReservation(courseId, user.uid, name);
             })
             .then((bookingResult) => {
               console.log(bookingResult);
+            })
+            .catch((error) => {
+              console.log("signInWithEmailAndPassword(Catch): " + error.code);
             });
+
           return;
+        } else if (error.code === "auth/invalid-credential") {
+          console.log("auth/invalid-credential: " + error.code);
         }
         console.log("Error from handleUserLogin: " + error.code);
       });
   } catch (e: any) {
-    console.log("e.code Error from handleUserLogin -->", e.code);
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log("User from handleUserLogin(Catch): " + user);
-        return user;
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        console.log(
-          "errorMessage from handleUserLogin(Catch): " + errorMessage
-        );
-      });
+    console.log("Error from handleUserLogin(Catch): ", e.code);
+    // signInWithEmailAndPassword(auth, email, password)
+    //   .then((userCredential) => {
+    //     const user = userCredential.user;
+    //     console.log("User from handleUserLogin(Catch): " + user);
+    //     return user;
+    //   })
+    //   .catch((error) => {
+    //     const errorMessage = error.message;
+    //     console.log(
+    //       "errorMessage from handleUserLogin(Catch): " + errorMessage
+    //     );
+    //   });
   }
 }
