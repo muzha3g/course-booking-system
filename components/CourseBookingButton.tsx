c2f56ed395a96a597c7851f27980aafc6d3af08e
Sangy <apple2951@gmail.com>
2025-06-14 16:14:48 +0800

    feat: Build courseBooking layout

--- components/CourseBookingButton.tsx
+++ components/CourseBookingButton.tsx
@@ -1,5 +1,51 @@
-import { Button } from "./ui/button";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function CourseBookingButton() {
-  return <Button className="w-24">Book</Button>;
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button className="w-24">Book</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Fill out form to book course</DialogTitle>
+            <DialogDescription>
+              Please enter tour details to book the course.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name">Real Name</Label>
+              <Input id="name" name="name" />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="phone-number">Phone Number</Label>
+              <Input id="phone-number" name="phone-number" />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Submit</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
 }
