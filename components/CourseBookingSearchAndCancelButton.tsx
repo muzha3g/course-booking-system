6e971619c2aa0870061b92be2d091042503ea4de
Sangy <apple2951@gmail.com>
2025-06-15 13:05:31 +0800

    feat: Validate data in CourseBookingSearchAndCancelButton

--- components/CourseBookingSearchAndCancelButton.tsx
+++ components/CourseBookingSearchAndCancelButton.tsx
@@ -14,30 +14,52 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
+import { z } from "zod";
 
 export default function CourseBookingSearchAndCancelButton() {
   const [phone, setPhone] = useState<string>("");
-  const handleSubmit = async () => {
-    if (phone.trim() === "") {
-      alert("please fill out all the fields");
-    } else {
-      setPhone("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const InputSchema = z.object({
+    phone: z
+      .string()
+      .min(1, { message: "Phone number is required." })
+      .regex(/^\d{10}$/, { message: "Phone number must be 10 digits." }),
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const validationResult = InputSchema.safeParse({ phone });
+
+    if (!validationResult.success) {
+      setErrorMessage(validationResult.error.errors[0].message);
+      console.log(
+        "Validation Error:",
+        validationResult.error.errors[0].message
+      );
+      return;
     }
+
+    console.log("Validation Success:", validationResult.data);
+    console.log("Form submitted successfully!");
+
+    try {
+    } catch {}
   };
 
   return (
     <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button variant={"outline"}>Search / Cancel Booking </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Search / Cancel Booking </DialogTitle>
-            <DialogDescription>
-              Please enter your phone number to search booking.
-            </DialogDescription>
-          </DialogHeader>
+      <DialogTrigger asChild>
+        <Button variant={"outline"}>Search / Cancel Booking </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Search / Cancel Booking </DialogTitle>
+          <DialogDescription>
+            Please enter your phone number to search booking.
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleSubmit} className="space-y-5">
           <div className="grid gap-4">
             <div className="grid gap-3">
               <Label htmlFor="phone-number">Phone Number</Label>
@@ -45,22 +67,25 @@ export default function CourseBookingSearchAndCancelButton() {
                 id="phone-number"
                 name="phone-number"
                 value={phone}
-                onChange={(e) => setPhone(e.target.value)}
+                className={errorMessage && "border-red-500"}
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                  setErrorMessage("");
+                }}
               />
+              {errorMessage && (
+                <p className="text-red-500 text-sm">{errorMessage}</p>
+              )}
             </div>
           </div>
           <DialogFooter>
             <DialogClose asChild>
               <Button variant="outline">Cancel</Button>
             </DialogClose>
-            <DialogClose asChild>
-              <Button type="submit" onClick={handleSubmit}>
-                Submit
-              </Button>
-            </DialogClose>
+            <Button type="submit">Submit</Button>
           </DialogFooter>
-        </DialogContent>
-      </form>
+        </form>
+      </DialogContent>
     </Dialog>
   );
 }
