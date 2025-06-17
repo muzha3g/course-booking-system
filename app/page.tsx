23808973d0498fd667bc316c67321dd700924895
Sangy <apple2951@gmail.com>
2025-06-17 18:53:14 +0800

    feat: Build home page to let user switch to two route

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-screen gap-6">
+      <Link
+        href="./user"
+        className="bg-slate-300 rounded-4xl text-2xl w-72 text-center py-3"
+      >
+        Booking Course
+      </Link>
+      <Link
+        href="./admin/login"
+        className="bg-gray-300 rounded-4xl text-2xl w-72 text-center py-3"
+      >
+        Admin DashBoard
+      </Link>
+    </div>
+  );
 }
