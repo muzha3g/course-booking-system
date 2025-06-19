de36d52254d5c33455e6da40c5298e1935a7f436
Sangy <apple2951@gmail.com>
2025-06-19 21:22:09 +0800

    feat: check token is exist or not in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,17 @@
 import { NextResponse, NextRequest } from "next/server";
-// import { getAuth } from "firebase/auth";
 
 export function middleware(request: NextRequest) {
-  const adminUid = "BbIaW4JydRg53yPj1qBwI2bx4Ma2";
   console.log("--- Middleware Started ---");
 
   if (
     request.nextUrl.pathname.startsWith("/admin") &&
     request.nextUrl.pathname !== "/admin/login"
   ) {
-    const userUid = request.cookies.get("user_uid")?.value;
+    const token = request.cookies.get("firebase_token")?.value;
 
-    if (userUid !== adminUid) {
-      console.log("userUid !== adminUid)");
+    if (!token) {
       return NextResponse.redirect(new URL("/admin/login", request.url));
     }
-
-    // const token = request.cookies.get("firebase_token")?.value;
-    // const decodedToken = getAuth()
-    //   .verifyIdToken(token)
-    //   .then((t) => t);
-
-    // console.log("decodedToken: ", decodedToken);
-
-    // if (!decodedToken.isAdmin) {
-    //   return NextResponse.redirect(new URL("/admin/login", request.url));
-    // }
   }
 
   return NextResponse.next();
